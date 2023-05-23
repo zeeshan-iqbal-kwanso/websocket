659c17e300a56898ad673bc5e300076078935029
zeeshan <zeeshan.iqbal@kwanso.com>
2023-05-23 11:38:50 +0500

    socket io implementation

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const expressWebSocket = require("express-ws");
-const websocket = require("websocket-stream");
-const websocketStream = require("websocket-stream/stream");
+// const expressWebSocket = require("express-ws");
+// const websocket = require("websocket-stream");
+// const websocketStream = require("websocket-stream/stream");
 const PORT = process.env.PORT || 3300;
 const hbs = require("hbs");
+const socketIO = require('socket.io');
+const io = socketIO(server);
 
 
 const app = express();
 
-expressWebSocket(app, null, {
-    perMessageDeflate: false
-});
+// expressWebSocket(app, null, {
+//     perMessageDeflate: false
+// });
 
 //app.engine("hbs", hbs());
 app.set("view engine", "hbs");
@@ -21,7 +23,7 @@ app.get("/", (request, response) => {
     response.render("home", {layout: false});
 });
 
-app.ws("/media", (ws, req) => {
+/*app.ws("/media", (ws, req) => {
     ws.send('Hello from the server!');
     ws.on('message', message => {
         console.log('Received message:', message);
@@ -31,8 +33,25 @@ app.ws("/media", (ws, req) => {
     ws.on('close', () => {
         console.log('Client disconnected');
     });
-});
+});*/
 
 const listener = app.listen(PORT, () => {
     console.log("Your app is listening on port " + listener.address().port);
+});
+
+// Handle socket connections
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle 'message' event
+    socket.on('message', (data) => {
+        console.log('Received message:', data);
+        // Broadcast the message to all connected clients
+        io.emit('message', data);
+    });
+
+    // Handle 'disconnect' event
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
